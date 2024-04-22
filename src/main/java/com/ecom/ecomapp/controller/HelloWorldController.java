ee0078593926c3bfd4933f94ab4e0291a14270ce
USERSAD\isrzouki <isrzouki@emeal.nttdata.com>
2024-04-23 00:22:01 +0200

    EcommApp-3: add authentication using oauth resource server with Firebase.

--- src/main/java/com/ecom/ecomapp/controller/HelloWorldController.java
+++ src/main/java/com/ecom/ecomapp/controller/HelloWorldController.java
@@ -1,5 +1,7 @@
 package com.ecom.ecomapp.controller;
 
+import java.security.Principal;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,5 +16,10 @@ public class HelloWorldController {
 	public String greeting() {
 		return "Hello World!";
 	}
+	
+	@GetMapping(path = "/user")
+    public String test(Principal principal) {
+        return principal.getName();
+    }
 	
 }
